1f9e20d46d44784097f3726f82e08aca9b0d4cb4
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-01-03 16:17:59 +0100

    delete everything

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f88ab5a1ec83fb230af7fd24fef0e19f'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '00e4d2d6-6a06-409b-a6e4-0f65e011cabe'
-};
